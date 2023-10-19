44a491e0f7487322acf2328717fcd3bc089ba900
MHashmi <maryam.hashmi@hotmail.co.uk>
2023-10-19 16:03:28 +0100

    modify code to check whether guessed letter is in the word

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,6 +1,16 @@
+import random
+
+# create list of words called word_list
+word_list = ['cherry' , 'watermelon' , 'mango' , 'strawberry' , 'banana' ]
+print(word_list)
+
+# computer chooses random word from word_list
+word = random.choice(word_list)
+print(word)
+
 # milestone 3 valid input checks from task 1 already implemented in milestone 2 during optimisation in task 6
 # removed is_valid_input function and ammended code to follow milestone 3 instructions   
- 
+
 while True :
     guess = input('Guess a single alphabetical letter : ')
     if len(guess) == 1 and guess.isalpha() == True :
@@ -9,4 +19,8 @@ while True :
         print("Invalid letter. Please, enter a single alphabetical character.")
 
 
+if guess in word :
+    print(f"Good guess! {guess} is in the word.")
+else : 
+    print(f"Sorry, {guess} is not in the word. Try again.")   
 
