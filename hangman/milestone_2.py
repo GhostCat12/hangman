833205ab1cf8a06d32129cb4fe46d3e47c3d5bca
MHashmi <maryam.hashmi@hotmail.co.uk>
2023-10-19 15:07:29 +0100

    adds function and creates whileloop to check if user input is valid

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,14 +1,23 @@
 import random
 
+# create list of words called word_list
 word_list = ['cherry' , 'watermelon' , 'mango' , 'strawberry' , 'banana' ]
 print(word_list)
 
+# computer chooses random word from word_list
 word = random.choice(word_list)
 print(word)
 
-guess = input('Guess a single letter : ')
+# function to check if user guess is valid
+def is_valid_input(guess) : 
+    while True : 
+        if len(guess) == 1 and guess.isalpha() == True :
+            print('Good guess!')
+            break
+        else : 
+            print('Try again!')
+            guess = input('Guess a single letter : ')
 
-if len(guess) == 1 and guess.isalpha() == True :
-    print('Good guess!')
-else : 
-    print('Try again!')
+
+guess = input('Guess a single letter : ')
+is_valid_input(guess)
