ce8aee6eed5126bd333fae1415b32a288cb8523f
MHashmi <maryam.hashmi@hotmail.co.uk>
2023-10-20 15:49:19 +0100

    fixes bug in ask_for_input and updates check_guess to replace unscores in word_guessed

--- milestone_4.py
+++ milestone_4.py
@@ -6,10 +6,8 @@ word_list = ['cherry' , 'watermelon' , 'mango' , 'strawberry' , 'banana' ]
 class Hangman:
     def __init__(self, word_list, num_lives=5):
         self.word = random.choice(word_list)
-        
         self.word_guessed = ['_'] * len(self.word)
-        print(self.word_guessed)
-        self.num_letters = len(self.word) #needs fix
+        self.num_letters = len(set(self.word)) #needs fix
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = list()
@@ -19,13 +17,23 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
+            #for letter in self.word:
+                #if letter == guess:
+            self.word_guessed[self.word.index(guess)] = guess
+            self.num_letters -= 1
+            print(self.num_letters, self.word_guessed)        
+
         else: 
             print(f"Sorry, {guess} is not in the word. Try again.") 
+
+
+
+
 
     def ask_for_input(self):
         while True:
             guess = input('Guess a single alphabetical letter : ')
-            if len(guess) != 1 and guess.isalpha() == False :
+            if (len(guess) != 1) or (guess.isalpha() == False) :
                 print("Invalid letter. Please, enter a single alphabetical character.")
             elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
