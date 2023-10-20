1a71d06e4a7ced159d7c0510bc34bbd1a5370956
MHashmi <maryam.hashmi@hotmail.co.uk>
2023-10-20 15:51:23 +0100

    cleans up check_guess code in milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -17,17 +17,10 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
-            #for letter in self.word:
-                #if letter == guess:
             self.word_guessed[self.word.index(guess)] = guess
-            self.num_letters -= 1
-            print(self.num_letters, self.word_guessed)        
-
+            self.num_letters -= 1        
         else: 
             print(f"Sorry, {guess} is not in the word. Try again.") 
-
-
-
 
 
     def ask_for_input(self):
